be839c23dd3ce1414aebfedf3a3c5ecd2e991bb1
Aziz Rozmetov <argringo97@gmail.com>
2024-06-07 19:54:01 +0500

    Updated

--- Program.cs
+++ Program.cs
@@ -3,8 +3,11 @@
 
 Console.Write("enter the first number: ");
 decimal firstNumber = Convert.ToInt16(Console.ReadLine());
-Console.Write("enter the second number:  ");
+Console.Write("enter the second number: ");
 decimal secondNumber = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("==========================");
+
 
 Console.WriteLine($"Addition  {firstNumber} + {secondNumber} = {firstNumber+secondNumber}");
 Console.WriteLine($"Subtraction {firstNumber} - {secondNumber} = {firstNumber-secondNumber}");
@@ -12,6 +15,8 @@ Console.WriteLine($"Multiplication {firstNumber} * {secondNumber} = {firstNumber
 Console.WriteLine($"Division {firstNumber} / {secondNumber} = {firstNumber/secondNumber}");
 Console.WriteLine($"Modulus {firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
 
+Console.WriteLine("==========================");
+
+
 Console.WriteLine($"Highest value {firstNumber} or {secondNumber} = {Math.Max(firstNumber, secondNumber)}");
 Console.WriteLine($"Lowest value  {firstNumber} or {secondNumber} = {Math.Min(firstNumber, secondNumber)}");
-Console.WriteLine($"Round {firstNumber} + {secondNumber} = {Math.Round(firstNumber/secondNumber)}");
